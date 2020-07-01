490f623ada5c15aac7138b426014188536047d69
tankyu2 <ssh_r1_16_2@ube-h.ysn21.jp>
2020-07-01 14:09:24 +0900

    Fixed import sqlalchemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 import os
 import random
